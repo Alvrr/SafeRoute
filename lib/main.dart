775f91815709eb9864c4957f86bfab53247d2410
FaridAlvaro <dekfarid2015@gmail.com>
2026-02-09 02:22:53 +0700

    web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCb928Hg5e5Q2HfyRaAruJ_TrFKIC29izc",
+        authDomain: "saferoute-129158.firebaseapp.com",
+        databaseURL:
+            "https://saferoute-129158-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "saferoute-129158",
+        storageBucket: "saferoute-129158.firebasestorage.app",
+        messagingSenderId: "295695171818",
+        appId: "1:295695171818:web:43462c8b377a17d254d793",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const SafeRouteApp());
 }
